20c20bd92b45d3fc7a4f4a73e042ab70755fd315
Calhovsky <pedromtm18@gmail.com>
2023-06-30 19:27:28 +0100

    Add user login views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,22 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
 
     def show 
-      @user = User.find(params[:id])
-      @articles = @user.articles
+      @articles = @user.articles.paginate(page: params[:page], per_page:5)
     end
   
+    def index
+      @users = User.paginate(page: params[:page], per_page:5)
+    end
 
     def new
       @user = User.new
     end
 
-    def edit
-      @user = User.find(params[:id])
-    end
-
     def create
       @user = User.new(user_params)
       if @user.save
+        session[:user_id] = @user.id
         flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed!"
         redirect_to articles_path
       else
@@ -24,11 +24,13 @@ class UsersController < ApplicationController
       end
     end
 
+    def edit
+    end
+
     def update
-      @user = User.find(params[:id])
       if @user.update(user_params)
         flash[:notice] = "Your account information was successfully updated"
-        redirect_to articles_path
+        redirect_to @user
       else
         render 'edit'
       end
@@ -40,6 +42,10 @@ class UsersController < ApplicationController
     private
     def user_params
       params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
     end
 
 end
